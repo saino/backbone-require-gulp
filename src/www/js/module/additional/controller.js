39ae1fa6be12e7807a4b9536543548fd1c3cb165
gyy <gyy@gli.cn>
2016-08-22 11:08:46 +0800

    提交添加附加险

---
+++ src/www/js/module/additional/controller.js
@@ -0,0 +1,14 @@
+// 文件名称: controllers.js
+//
+// 创 建 人: guyy
+// 创建日期: 2016/8/22
+// 描    述: controllers.js
+define([
+    'module/additional/views/additionalView'
+],function(AdditionalView){
+    return {
+        plan : function(){
+            app.page.show(AdditionalView);
+        }
+    };
+});
---
+++ src/www/js/module/additional/views/additionalView.js
@@ -0,0 +1,13 @@
+/**
+ * Created by GYY on 2016/8/22.
+ * 添加附加险页面
+ */
+define([
+    'common/base/base_view',
+    'text !module/additional/templates/addition'
+],function(BaseView, AdditionalTpl){
+    var AdditionalView = BaseView.extend({
+
+    });
+    return AdditionalView;
+});
