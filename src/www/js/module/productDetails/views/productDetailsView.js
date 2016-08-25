8c5722ae3e60f857253dcde058b85d0c3c2ce2a5
fish <fishYu@gli.cn>
2016-08-25 16:29:29 +0800

    完善静态页面

--- src/www/js/module/productDetails/views/productDetailsView.js
+++ src/www/js/module/productDetails/views/productDetailsView.js
@@ -11,7 +11,8 @@ define([
         id:"product-details-container",
         ui:{
             "topCon":".top-title",
-            "btnBack":".top-title-left" //点击返回
+            "btnBack":".top-title-left", //点击返回
+            "productDetailsMain" : "#product-details-main"
         },
         events:{
             "tap @ui.btnBack":"onBtnBackHandler"
@@ -24,6 +25,11 @@ define([
             if(device.ios()){
                 self.ui.topCon.css("padding-top",utils.toolHeight+"px");
             }
+            setTimeout(function(){
+                var height = self.ui.topCon.outerHeight(true);
+                self.ui.productDetailsMain.css({height: "calc(100% - " + height + "px)"});
+            }, 0)
+
         },
         pageIn:function(){
 
