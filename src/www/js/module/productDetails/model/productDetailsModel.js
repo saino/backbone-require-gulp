ba546dc09b8315b901ea5d4bce97518cba7ac806
fish <fishYu@gli.cn>
2016-08-26 15:33:39 +0800

    修改产品详情为动态数据

--- src/www/js/module/productDetails/model/productDetailsModel.js
+++ src/www/js/module/productDetails/model/productDetailsModel.js
@@ -1,46 +1,218 @@
-/**
- * 简单模块通用数据类 
- * add by guYY 2016/8/24
- */
-define([
-    'backbone'
-],function(){
-    var detailModel = Backbone.Model.extend({
-        constructor:function(){
-            Backbone.Model.apply(this,arguments);
+// 文件名称: productDetailsModel
+//
+// 创 建 人: fishYu
+// 创建日期: 2016/8/25 18:46
+// 描    述: 产品详情数据对象
+"use strict";
+define([], function () {
+    var ProductDetailsModel = function () {
+    };
+
+    var test = {
+        "status": "0",
+        "errorMessages": [],
+        "packageId": 9100001,
+        "packageName": "华夏健康人生计划",
+        "company": {
+            "listId": 3,
+            "companyName": "华夏人寿保险股份有限公司",
+            "organLogo": "./images/temp/hx-logo.png",
+            "abbrName": "华夏",
+            "organDesc": null,
+            "telephone": "95300",
+            "url": null
         },
-        initialize:function(){
-            var self = this;
-            self.set({"title":"","detailContent":"","hasShare":false,"isIos":device.ios()});//是否函分享按钮
-            if(device.ios()){
-                self.set({"paddingTop":'style="padding-top:'+utils.toolHeight+'px"',"conHeight":'style="height:-webkit-calc(100% - '+(utils.toolHeight+85)+'px)"'});
-            }else{
-                self.set({"paddingTop":"","conHeight":""});
-            }
+        "totalVisitCount": 999,
+        "amountLimit": {   //保额限制
+            "minAmount": 1000000,
+            "maxAmount": 9999999999999,
+            "moneyId": 1,
+            "limitUnit": "元"
         },
-        //标题 如病种详情、公司介绍等        
-        setTitle:function(title){
-            if(title && title != ""){
-                this.set({"title":title});
-            }else{
-                this.set({"title":""});
-            }
+        "ageRange": {          //年龄区间
+            "minAge": 1,
+            "minUnit": "天",
+            "maxAge": 999,
+            "maxUnit": "周岁"
         },
-        //内容详情
-        setDetail:function(detailContent){
-            if(detailContent && detailContent != ""){
-                this.set({"detailContent":detailContent});
-            }else{
-                this.set({"detailContent":""});
-            }
-        },
-        /**
-         * 设置是否可分享收藏
-         * share :boolean
-         */
-        setHasShare:function(share){
-            this.set({"hasShare":share});
+        "prdtTermChargeList": [{      //费用期间
+            "periodType": "3",
+            "periodValue": 1      //期限值
+        }, {      //费用期间
+            "periodType": "3",
+            "periodValue": 5      //期限值
+        }, {      //费用期间
+            "periodType": "3",
+            "periodValue": 15      //期限值
+        }, {      //费用期间
+            "periodType": "3",
+            "periodValue": 20      //期限值
+        }],
+        "prdtTermCoverageList": [
+            { //保障期间
+                "periodType": "3",
+                "periodValue": 60
+            }, { //保障期间
+                "periodType": "3",
+                "periodValue": 70
+            }, {
+                "periodType": "3",
+                "periodValue": 20
+            }, {
+                "periodType": "3",
+                "periodValue": 30
+            }],
+        "salesRightsList": [{
+            "rightId": 1,
+            "rightName": "终身寿险",
+            "rightDesc": "终身寿险",
+            "displayOrder": 1
+        }, {
+            "rightId": 4,
+            "rightName": "单次重疾",
+            "rightDesc": null,
+            "displayOrder": 2
+        }, {
+            "rightId": 5,
+            "rightName": "多次轻症",
+            "rightDesc": null,
+            "displayOrder": 3
+        }, {
+            "rightId": 9,
+            "rightName": "重疾豁免",
+            "rightDesc": null,
+            "displayOrder": 6
+        }, {
+            "rightId": 10,
+            "rightName": "轻症豁免",
+            "rightDesc": null,
+            "displayOrder": 7
+        }, {
+            "rightId": 28,
+            "rightName": "投保豁免",
+            "rightDesc": null,
+            "displayOrder": 4
+        }, {
+            "rightId": 29,
+            "rightName": "被保豁免",
+            "rightDesc": null,
+            "displayOrder": 5
+        }],
+        "productFeatureList": [{      //特色
+            "listId": 100001,
+            "featurePic": "./images/temp/range.png",
+            "displayOrder": 1,
+            "packageId": 9100001
+        }, {
+            "listId": 100002,
+            "featurePic": "./images/temp/long.png",
+            "displayOrder": 2,
+            "packageId": 9100001
+        }, {
+            "listId": 100003,
+            "featurePic": "./images/temp/limit.png",
+            "displayOrder": 3,
+            "packageId": 9100001
+        }],
+        "salesLiabilityList": [        //责任
+            {
+                "liabId": 2016001,
+                "displayOrder": 1,
+                "liabName": "身故赔付已交保费",
+                "liabDesc": "未满18周岁，因意外或者90天等待期后因非意外导致身故，给付1.5倍保费，或者90天等待期内因非意外导致身故，给付已交保费；18周岁后，因意外或者90天等待期后因非意外导致身故，给付保额，或者90天等待期内因非意外导致身故，给付已交保费。",
+                "liabType": null
+            },
+            {
+                "liabId": 2016002,
+                "displayOrder": 2,
+                "liabName": "全残赔付已交保费",
+                "liabDesc": "未满18周岁，因意外或者90天等待期后因非意外导致身故，给付1.5倍保费，或者90天等待期内因非意外导致身故，给付已交保费；18周岁后，因意外或者90天等待期后因非意外导致身故，给付保额，或者90天等待期内因非意外导致身故，给付已交保费。",
+                "liabType": null
+            },
+            {
+                "liabId": 2016003,
+                "displayOrder": 3,
+                "liabName": "<em style='color:#e39b2a'>33</em>种轻症疾病额外赔付保险金<em style='color:#e39b2a'>20%</em>，最多<em style='color:#e39b2a'>5</em>次",
+                "liabDesc": "未满18周岁，因意外或者90天等待期后因非意外导致身故，给付1.5倍保费，或者90天等待期内因非意外导致身故，给付已交保费；18周岁后，因意外或者90天等待期后因非意外导致身故，给付保额，或者90天等待期内因非意外导致身故，给付已交保费。",
+                "liabType": null
+            },
+            {
+                "liabId": 2016004,
+                "displayOrder": 4,
+                "liabName": "<em style='color:#e39b2a'>77</em>种重疾赔付保险金",
+                "liabDesc": "未满18周岁，因意外或者90天等待期后因非意外导致身故，给付1.5倍保费，或者90天等待期内因非意外导致身故，给付已交保费；18周岁后，因意外或者90天等待期后因非意外导致身故，给付保额，或者90天等待期内因非意外导致身故，给付已交保费。",
+                "liabType": null
+            },
+            {
+                "liabId": 2016005,
+                "displayOrder": 5,
+                "liabName": "疾病终末期赔付已交保费",
+                "liabDesc": "未满18周岁，因意外或者90天等待期后因非意外导致身故，给付1.5倍保费，或者90天等待期内因非意外导致身故，给付已交保费；18周岁后，因意外或者90天等待期后因非意外导致身故，给付保额，或者90天等待期内因非意外导致身故，给付已交保费。",
+                "liabType": null
+            },
+            {
+                "liabId": 2016006,
+                "displayOrder": 6,
+                "liabName": "轻疾病豁免保费",
+                "liabDesc": "未满18周岁，因意外或者90天等待期后因非意外导致身故，给付1.5倍保费，或者90天等待期内因非意外导致身故，给付已交保费；18周岁后，因意外或者90天等待期后因非意外导致身故，给付保额，或者90天等待期内因非意外导致身故，给付已交保费。",
+                "liabType": null
+            },
+            {
+                "liabId": 2016007,
+                "displayOrder": 7,
+                "liabName": "投保人患<em style='color:#e39b2a'>77</em>种重疾豁免后期保险费",
+                "liabDesc": "未满18周岁，因意外或者90天等待期后因非意外导致身故，给付1.5倍保费，或者90天等待期内因非意外导致身故，给付已交保费；18周岁后，因意外或者90天等待期后因非意外导致身故，给付保额，或者90天等待期内因非意外导致身故，给付已交保费。",
+                "liabType": null
+            },
+            {
+                "liabId": 2016008,
+                "displayOrder": 8,
+                "liabName": "投保人患<em style='color:#e39b2a'>33</em>种轻疾豁免后期保险费",
+                "liabDesc": "未满18周岁，因意外或者90天等待期后因非意外导致身故，给付1.5倍保费，或者90天等待期内因非意外导致身故，给付已交保费；18周岁后，因意外或者90天等待期后因非意外导致身故，给付保额，或者90天等待期内因非意外导致身故，给付已交保费。",
+                "liabType": null
+            }, {
+                "liabId": 2016009,
+                "displayOrder": 9,
+                "liabName": "投保人身故豁免后期保险费",
+                "liabDesc": "未满18周岁，因意外或者90天等待期后因非意外导致身故，给付1.5倍保费，或者90天等待期内因非意外导致身故，给付已交保费；18周岁后，因意外或者90天等待期后因非意外导致身故，给付保额，或者90天等待期内因非意外导致身故，给付已交保费。",
+                "liabType": null
+            }, {
+                "liabId": 20160010,
+                "displayOrder": 10,
+                "liabName": "投保人全残豁免后期保险费",
+                "liabDesc": "未满18周岁，因意外或者90天等待期后因非意外导致身故，给付1.5倍保费，或者90天等待期内因非意外导致身故，给付已交保费；18周岁后，因意外或者90天等待期后因非意外导致身故，给付保额，或者90天等待期内因非意外导致身故，给付已交保费。",
+                "liabType": null
+            }],
+        "attachProductList": [{        //附加险
+            "salesProductId": 100003,
+            "salesProductName": "华夏附加住院费用补偿医疗2014",
+            "insType": "2"
+        }],
+        "packageList": [{     //组合列表
+            "salesProductId": 100001,
+            "salesProductName": "华夏健康人生重大疾病保险",
+            "insType": "1"
+        }, {
+            "salesProductId": 100002,
+            "salesProductName": "华夏附加投保人豁免保费重大疾病保险",
+            "insType": "0"
+        }]
+    };
+
+    /**
+     * 知识搜索
+     * @param currentUserId  当前用户ID
+     * @param salesPackageId  售卖的保险ID
+     * @param cb_ok
+     * @param cb_err
+     */
+    ProductDetailsModel.prototype.getProductInfo = function (currentUserId, salesPackageId, cb_ok, cb_err) {
+        if (cb_ok) {
+            cb_ok(test);
         }
-    });
-    return detailModel;
+    };
+
+
+    var productDetailsModel = new ProductDetailsModel();
+    return productDetailsModel;
 });
--- src/www/js/module/productDetails/views/productDetailsView.js
+++ src/www/js/module/productDetails/views/productDetailsView.js
@@ -5,16 +5,25 @@
 define([
     'common/base/base_view',
     'text!module/productDetails/templates/productDetails.html',
+    'module/productDetails/model/productDetailsModel',
     "msgbox"
-],function(BaseView, Tpl, MsgBox){
+],function(BaseView, Tpl, productDetailsModel, MsgBox){
     var ProductDetailsView = BaseView.extend({
         template: _.template(Tpl),
         id:"product-details-container",
+        currentUserId : "",     //当前用户ID
+        productId : "",         //售卖产品ID
         ui:{
             topCon : ".top-title",
             backBtn : ".top-title-left", //点击返回
             shareBtn : ".top-title-right-2",              //分享按钮
             collectBtn : ".top-title-right-1",            //收藏按钮
+            detailsInfoTop : ".details-info-top",       // 产品名称和公司LOGO
+            productDetailsPv : ".product-details-pv",   //产品PV
+            insureAge : "#insure-age span:last-child",                      //投保年龄
+            limitCoverage : "#limit-coverage span:last-child",             //最低保额
+            paymentRange : "#payment-range span:last-child",             //交费期间
+            safeguardRange : "#safeguard-range span:last-child",             //保障期间
             productDetailsMain : "#product-details-main",
             summaryContent : ".insure-summary-content" ,     //保障概览
             featureContent : ".insure-feature-content",     //产品特色
@@ -54,11 +63,206 @@ define([
 
             self.productId = self.getOption("productId");   //获取产品ID
             console.log(self.productId);
+            //根据用户ID 和保险售卖ID查询数据
+            self._initView = self.initView.bind(self);
+            productDetailsModel.getProductInfo(self.currentUserId, self.productId, self._initView, function(err){
+                console.log(err);
+            });
+
 
         },
         pageIn:function(){
             var self = this;
 
+        },
+        /**
+         * 初始化界面的动态数据
+         * @param data
+         */
+        initView : function(data){
+            var self = this;
+            //设置用户信息
+            var packageName = data.packageName; //保险名称
+            var organLogo =  data.company.organLogo;    //公司logo
+            var totalVisitCount = data.totalVisitCount; //总PV数
+            self.initInfoView(packageName, organLogo, totalVisitCount);
+            //设置投保条件
+            var amountLimit = data.amountLimit; //保额限制
+            var ageRange = data.ageRange; //年龄区间
+            var prdtTermChargeList = data.prdtTermChargeList; //费用期间
+            var prdtTermCoverageList = data.prdtTermCoverageList; //保障期间
+            self.initConditionView(amountLimit, ageRange, prdtTermChargeList, prdtTermCoverageList);
+            //设置保障概览
+            var salesRightsList = data.salesRightsList; //保障概览
+            self.initSummaryView(salesRightsList);
+            //设置产品特色
+            var productFeatureList = data.productFeatureList; //产品特色
+            self.initFeatureView(productFeatureList);
+            //设置保险责任
+            var salesLiabilityList = data.salesLiabilityList;
+            self.initDutyView(salesLiabilityList);
+            //组合计划
+            var packageList = data.packageList;
+            self.initPlanView(packageList);
+            //附加险
+            var attachProductList = data.attachProductList;
+            self.initSubjoinView(attachProductList);
+        },
+        /**
+         * 初始化投保信息
+         * @param packageName
+         * @param organLogo
+         * @param totalVisitCount
+         */
+        initInfoView : function(packageName, organLogo, totalVisitCount){
+            var self = this;
+            self.ui.detailsInfoTop.text(packageName);
+            self.ui.detailsInfoTop.css({"background" : "url("+organLogo+") 100% center no-repeat"});
+            self.ui.productDetailsPv.text(totalVisitCount);
+        },
+        /**
+         * 初始化投保条件
+         * @param amountLimit   obj{}
+         * @param ageRange  obj{}
+         * @param prdtTermChargeList [obj,obj]
+         * @param prdtTermCoverageList [obj,obj]
+         */
+        initConditionView : function (amountLimit, ageRange, prdtTermChargeList, prdtTermCoverageList){
+            var self = this;
+            //投保年龄
+            var minAge = ageRange.minAge;       //最小年龄
+            var minAgeUnit = ageRange.minUnit;  //最小年龄单位
+            var maxAge = ageRange.maxAge;       //最小年龄
+            var maxAgeUnit = ageRange.maxUnit;  //最小年龄单位
+            self.ui.insureAge.text(minAge+minAgeUnit+"-"+maxAge+maxAgeUnit);
+            //最低保额
+            var minAmount = amountLimit.minAmount;      //最低保额
+            var limitUnit = amountLimit.limitUnit;      //保额单位
+            self.ui.limitCoverage.text(minAmount+limitUnit);
+            //交费期间
+            var paymentStr = "";
+            for(var i = 0; i < prdtTermChargeList.length; i++){
+                var obj1 = prdtTermChargeList[i];
+                if(i != prdtTermChargeList.length -1){
+                    paymentStr += obj1.periodValue + "/"
+                }else{
+                    paymentStr += obj1.periodValue;
+                }
+            }
+            self.ui.paymentRange.text(paymentStr+"年交");
+            //保障期间
+            var safeguardStr = "";
+            for(var j = 0; j < prdtTermCoverageList.length; j++){
+                var obj2 = prdtTermCoverageList[j];
+                if(j == 0){
+                    safeguardStr += "至"+obj2.periodValue+"岁，"
+                }else{
+                    safeguardStr += obj2.periodValue+"年，"
+                }
+            }
+            self.ui.safeguardRange.text(safeguardStr + "终身");
+        },
+        /**
+         * 设置保障概览
+         * @param salesRightsList  保障列表
+         */
+        initSummaryView : function(salesRightsList) {
+            var self = this;
+            var summaryTemp = '<div class="insure-summary-item">{rightName}</div>';
+            var summaryStr = "";
+            for(var i = 0; i < salesRightsList.length; i++){
+                var obj = salesRightsList[i];
+                var realTemp = summaryTemp.replace("{rightName}", obj.rightName);
+                summaryStr += realTemp;
+            }
+            summaryStr += '<div class="clear"></div>';      //最后添加一个清除浮动
+            self.ui.summaryContent.html(summaryStr);
+        },
+        /**
+         * 设置产品特色
+         * @param productFeatureList  特色列表
+         * <div class="insure-feature-item">
+         * <img  src="./images/insurance/long.png"/>
+         * <span>长期保障期限：各项责任统统保障终身</span>
+         * </div>
+         */
+        initFeatureView : function (productFeatureList) {
+            var self = this;
+            var featureTemp = '<div class="insure-feature-item" style="background: url({feature-pic}) no-repeat center"></div>';
+            var featureStr = "";
+            for(var i = 0; i < productFeatureList.length; i++){
+                var obj = productFeatureList[i];
+                var realTemp = featureTemp.replace("{feature-pic}", obj.featurePic);
+                featureStr += realTemp;
+            }
+            self.ui.featureContent.html(featureStr);
+        },
+        /**
+         * 设置产品特色
+         * @param salesLiabilityList  特色列表
+         * <div class="insure-duty-item">
+         *<div class="duty-item-title on">
+         *<span>身故赔付已交保费</span>
+         *<span class="pull-icon-small"></span>
+         *</div>
+         *<div class="duty-item-content">
+         *未满18周岁，因意外或者90天等待期后因非意外导致身故，给付1.5倍保费，或者90天等待期内因非意外导致身故，给付已交保费；
+         *18周岁后，因意外或者90天等待期后因非意外导致身故，给付保额，或者90天等待期内因非意外导致身故，给付已交保费。
+         *</div>
+         *</div>
+         */
+        initDutyView : function (salesLiabilityList) {
+            var self = this;
+            var dutyTemp = '<div class="insure-duty-item">'+
+                    '<div class="duty-item-title">'+
+                    '<span>{liabName}</span>'+
+                    '<span class="pull-icon-small"></span>'+
+                    '</div>'+
+                    '<div class="duty-item-content">{liabDesc}</div>'+
+                    '</div>';
+            var dutyStr = "";
+            for(var i = 0; i < salesLiabilityList.length; i++){
+                var obj = salesLiabilityList[i];
+                var realTemp = dutyTemp.replace("{liabName}", obj.liabName).replace(/\{liabDesc\}/g, obj.liabDesc);
+                dutyStr += realTemp;
+            }
+            self.ui.dutyContent.html(dutyStr);
+        },
+        /**
+         * 设置组合计划险
+         * @param packageList
+         */
+        initPlanView : function (packageList){
+            var self = this;
+            var planTemp = '<div class="insure-plan-item" data-type="{salesProductId}">'+
+                '<span data-type="{salesProductId}">{salesProductName}</span>'+
+                '<span class="pull-icon-next" data-type="{salesProductId}"></span>'+
+            '</div>';
+            var planStr = "";
+            for(var i = 0; i < packageList.length; i++){
+                var obj = packageList[i];
+                var realTemp = planTemp.replace("{salesProductName}", obj.salesProductName).replace(/\{salesProductId\}/g, obj.salesProductId);
+                planStr += realTemp;
+            }
+            self.ui.planContent.html(planStr);
+        },
+        /**
+         * 设置附加险
+         * @param attachProductList
+         */
+        initSubjoinView : function (attachProductList){
+            var self = this;
+            var subjoinTemp = '<div class="insure-subjoin-item" data-type="{salesProductId}">'+
+                '<span data-type="{salesProductId}">{salesProductName}</span>'+
+                '<span class="pull-icon-next" data-type="{salesProductId}"></span>'+
+                '</div>';
+            var subjoinStr = "";
+            for(var i = 0; i < attachProductList.length; i++){
+                var obj = attachProductList[i];
+                var realTemp = subjoinTemp.replace("{salesProductName}", obj.salesProductName).replace(/\{salesProductId\}/g, obj.salesProductId);
+                subjoinStr += realTemp;
+            }
+            self.ui.subjoinContent.html(subjoinStr);
         },
         /**
          * 点击返回
