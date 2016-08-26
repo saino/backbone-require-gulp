c85ef7f677031d2ea580ead4533c3cf4a62d1b7b
刘标 <saino@gli.cn>
2016-08-26 18:55:40 +0800

    添加默认排序浮层

--- src/www/js/module/lifeInsurance/views/lifeInsurance.js
+++ src/www/js/module/lifeInsurance/views/lifeInsurance.js
@@ -11,12 +11,17 @@ define([
 
         ui: {
             back: "#top-title-left",
-            productInsureDuty: ".product-insure-duty"
+            productInsureDuty: ".product-insure-duty",
+            searchDefaultSort: "#search-default-sort",
+            defaultSortLayoutFloat: "#default-sort-layout-float",
+            defaultSortContent: ".default-sort-content"
         },
 
         events: {
             "tap @ui.back": "clickBackHandler",
-            "tap @ui.productInsureDuty": "clickProductInsureDutyHandler"
+            "tap @ui.productInsureDuty": "clickProductInsureDutyHandler",
+            "tap @ui.searchDefaultSort": "clickSearchDefaultSortHandler",
+            "tap @ui.defaultSortContent": "clickDefaultSortContentHandler"
         },
 
         clickBackHandler: function (event) {
@@ -45,6 +50,28 @@ define([
             if(parent){
                parent.next().slideToggle();
             }
+        },
+        clickSearchDefaultSortHandler: function(event){
+            event.stopPropagation();
+            event.preventDefault();
+
+            this.ui.defaultSortLayoutFloat.show();
+
+        },
+        clickDefaultSortContentHandler: function(event){
+            event.stopPropagation();
+            event.preventDefault();
+            var self = this;
+
+            //self.ui.defaultSortLayoutFloat.hidden();
+
+            var target = event.target;
+            var $target = $(target);
+            if($target.attr("class") == "default-sort-item"){
+                self.ui.defaultSortLayoutFloat.find(".default-sort-item-selected").attr("class", "default-sort-item");
+                $target.attr("class","default-sort-item default-sort-item-selected");
+            }
+            self.ui.defaultSortLayoutFloat.hide();
         }
     });
 });
