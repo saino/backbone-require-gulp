d44e5c22be7f9469cb27df70233e88b475f15009
zhao <zhao_5812@hotmail.com>
2016-08-31 18:00:29 +0800

    产品条款

---
+++ src/www/js/module/clause/model/clauseModel.js
@@ -0,0 +1,35 @@
+// 文件名称: productDetailsModel
+//
+// 创 建 人: fishYu
+// 创建日期: 2016/8/25 18:46
+// 描    述: 产品详情数据对象
+"use strict";
+define([], function () {
+    var ClauseModel = function () {
+    };
+
+    var p = ClauseModel.prototype;
+
+    p.getTermInfo = function(productId, cb_ok, cb_err){
+        var opt = {};
+        opt.url = "/dt/productService/getTermInfo";
+        opt.type = "POST";
+        var data = {};
+        data.product = productId;
+        opt.data = data;
+        opt.success = function(result){
+            if(result.status == 0) {
+                if(cb_ok) cb_ok(result);
+            }else {
+                if(cb_err) cb_err(result.errorMessages);
+            }
+        };
+        opt.error = function(err){
+            if(cb_err) cb_err(err);
+        };
+        utils.requestData(opt);
+    };
+
+    var clauseModel = new ClauseModel();
+    return clauseModel;
+});
--- src/www/js/module/clause/views/clauseView.js
+++ src/www/js/module/clause/views/clauseView.js
@@ -4,14 +4,16 @@
  */
 define([
     'common/base/base_view',
-    'text!module/clause/templates/clause.html'
-],function(BaseView, ClauseTpl){
+    'text!module/clause/templates/clause.html',
+    'module/clause/model/clauseModel'
+],function(BaseView, ClauseTpl, clauseModel){
     var ClauseView = BaseView.extend({
         template: _.template(ClauseTpl),
         id:"clause-container",
         ui:{
             "topCon":"#top-title",
-            "btnBack":"#top-title-left" //点击返回
+            "btnBack":"#top-title-left", //点击返回
+            infoDiv : "#clause-main"
         },
         events:{
             "tap #top-title-left":"_clickBackHandler"
@@ -24,7 +26,20 @@ define([
             if(device.ios()){
                 self.ui.topCon.css("padding-top",utils.toolHeight+"px");
             }
+
+            var productId = self.getOption("productId");
+            clauseModel.getTermInfo(productId, function(data){
+                self.initData(data);
+            }, function(){
+
+            })
         },
+
+        initData : function(data){
+            this.ui.infoDiv.html(data.itemDesc);
+        },
+
+
         pageIn:function(){},
         //点击返回
         _clickBackHandler:function(e){
