d2f975e2158bc9c15d073a08d6af32204ececc5b
guyy0423 <guyywan_1314@163.com>
2016-10-03 17:58:36 +0800

    修改公司介绍菜单不好点的问题

--- src/www/js/utils/utils.js
+++ src/www/js/utils/utils.js
@@ -4,7 +4,7 @@
 (function(window){
     var utils = {};
     window.utils = utils;
-    utils.isDebug = true;//true 原生   false 浏览器
+    utils.isDebug = true;//true 原生   faplse 浏览器
     var href = window.location.href;
 //    if(href.indexOf("210.13.77.75") >= 0){
 //        utils.isDebug = true;
